27481f32b205bc2a4deb80907977bed32708f1fe
Lars Knudsen <larsgk@gmail.com>
2018-07-10 20:45:43 +0200

    Early web app manifest

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,9 +1,11 @@
 module.exports = {
     staticFileGlobs: [
+        'assets/images/**',
         'assets/audio/piano_c.ogg',
         'src/**',
         'local_modules/**',
         'modules/**',
-        'index.html'
+        'index.html',
+        'manifest.json'
     ]
 };
